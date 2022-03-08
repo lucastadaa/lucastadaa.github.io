38a2691f621864450f5e7a50383afb5e898205ff
lucastadaa <99166862+lucastadaa@users.noreply.github.com>
2022-03-08 10:18:18 +0200

    Add files via upload

--- demo8/kirjautuminen.js
+++ demo8/kirjautuminen.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", onkoKirjautunut);
 
 
-localStorage.getElementById("kirjaudu_ulos").style.display = "none";
-
 
 function onkoKirjautunut() {
+    document.getElementById("ulos_kirjautuminen").style.display = "none";
     let kirjautunut = 'kylla';
 if (localStorage.getItem("kirjautunut") === "kylla") {
     document.getElementById('tervetuloa_teksti').textContent += localStorage.getItem("nimi");
     document.getElementById("kirjautumis_lomake").style.display = "none";
+    document.getElementById("ulos_kirjautuminen").style.display = "initial";
     }
 }
 
